5428941369d5dbf14e30a8f8978caa87382f3c87
naufaladrian <naufal.adrian904@gmail.com>
2025-02-18 15:30:37 +0700

    add origin

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
