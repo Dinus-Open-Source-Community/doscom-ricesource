164d3cac4da688a8916092f74112eb7a1d29bfbc
naufaladrian <naufal.adrian904@gmail.com>
2025-02-23 16:56:07 +0700

    add user type

--- frontend/types.ts
+++ frontend/types.ts
@@ -1,29 +1,38 @@
 export interface Rice {
-    id: number;
-    judul: string;
-    description: string;
-    like: number;
-    image_url: string | null;
-    github: string | null;
-    desktop_environment: string | null;
-    windows_manager: string | null;
-    distro: string | null;
-    terminal: string | null;
-    shell: string | null;
-    author: string | null;
-    user_id: number | null;
-    created_at: string | null;
-    updated_at: string | null;
+  id: number;
+  judul: string;
+  description: string;
+  like: number;
+  image_url: string | null;
+  github: string | null;
+  desktop_environment: string | null;
+  windows_manager: string | null;
+  distro: string | null;
+  terminal: string | null;
+  shell: string | null;
+  author: string | null;
+  user_id: number | null;
+  created_at: string | null;
+  updated_at: string | null;
 }
 
 export interface Comment {
-    id: number;
-    description: string;
-    parent_id: number | null;
-    id_user: number;
-    created_at: string;
-    user: {
-        avatar: string;
-        username: string;
-    };
+  id: number;
+  description: string;
+  parent_id: number | null;
+  id_user: number;
+  created_at: string;
+  user: {
+    avatar: string;
+    username: string;
+  };
+}
+
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  is_active: boolean;
+  created_at: string;
+  avatar: string;
 }
