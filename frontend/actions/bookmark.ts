85c59f1b0859d9d4a15d1e5b0b019dceacc4de34
naufaladrian <naufal.adrian904@gmail.com>
2025-02-25 09:30:38 +0700

    handle auth on getAllBookmarks

--- frontend/actions/bookmark.ts
+++ frontend/actions/bookmark.ts
@@ -1,17 +1,26 @@
-import { Rice } from "@/types";
+import { Bookmark, Rice } from "@/types";
 import axios from "axios";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/bookmarks`;
 
-export async function getAllBookmarks(token: string): Promise<Rice[]> {
-  const res = await axios.get(`${URL}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return res.data;
+export async function getAllBookmarks(token: string): Promise<Bookmark[]> {
+  try {
+    const response = await axios.get(`${URL}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response?.status === 401) {
+        throw new Error("Unauthorized");
+      }
+    }
+    console.error("Error fetching bookmarks:", error);
+    throw new Error("Failed to fetch bookmarks");
+  }
 }
-
 export async function bookmarkConfig(id: string, token: string): Promise<void> {
   await axios.post(
     `${URL}`,
