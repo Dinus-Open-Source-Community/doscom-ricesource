38b8ba43610b3a9202d42684078292846150bff5
naufaladrian <naufal.adrian904@gmail.com>
2025-02-27 12:56:35 +0700

    store token to cookie

--- frontend/actions/auth.ts
+++ frontend/actions/auth.ts
@@ -1,3 +1,5 @@
+"use server"
+import { cookies } from 'next/headers'
 import axios from "axios";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/userauth`;
@@ -11,11 +13,19 @@ interface registerProps {
     username: string;
     email: string;
     password: string;
-    avatar : string;
+    avatar: string;
 }
 
 export async function login(data: loginProps) {
     const response = await axios.post(`${URL}/login`, data);
+    const cookieStore = await cookies();
+
+    // Set the cookie correctly
+    cookieStore.set("token", response.data.token, {
+        httpOnly: true,
+        maxAge: 86400, // 1 day
+        path: "/",
+    });
+
     return response.data;
 }
-
