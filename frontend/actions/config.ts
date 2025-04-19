bf1bceecdb70271d0808fb50a15cec3f89aabf18
MFarisA <muhfarisassami@gmail.com>
2025-04-19 13:55:50 +0700

    feat: add fetchConfigData and fetchUserByAdmin functions; implement deleteUserByAdmin function; update package dependencies

---
+++ frontend/actions/config.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"; // Fallback for development
+
+export interface Config {
+    id: number;
+    judul: string;
+    description: string;
+    like: number;
+    image_url: string;
+    github: string;
+    desktop_environment: string;
+    widowss_manager: string;
+    distro: string;
+    terminal: string;
+    shell: string;
+    author: string;
+}
+
+export async function fetchConfigData(params?: Partial<Config>) {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        console.error('No token found in localStorage');
+        return;
+    }
+    const response = await axios.get(`${URL}/configs`, {
+        params: params,
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    console.log(response.data)
+    return response.data
+}
---
+++ frontend/actions/userByAdmin.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"; // Fallback for development
+
+export interface UserbyAdmin {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    avatar: string;
+  }
+
+
+export async function fetchUserByAdmin(params?: Partial<UserbyAdmin>) {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        console.error('No token found in localStorage');
+        return;
+    }
+    const response = await axios.get(`${URL}/userauth`, {
+        params: params,
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    console.log(response.data)
+    return response.data
+}
+
+export async function deleteUserByAdmin(id: number) {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        console.error('No token found in localStorage');
+        return;
+    }
+    const response = await axios.delete(`${URL}/userauth/${id}`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    return response.data
+}
