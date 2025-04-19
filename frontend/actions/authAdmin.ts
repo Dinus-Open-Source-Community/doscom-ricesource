b0665326a5a835bdeb4ed74fbac7cc2824cb1ebd
MFarisA <muhfarisassami@gmail.com>
2025-04-19 13:51:24 +0700

    feat: add deleteAdmin function and improve error handling in admin actions; enhance delete confirmation dialog styling

--- frontend/actions/authAdmin.ts
+++ frontend/actions/authAdmin.ts
@@ -18,6 +18,7 @@ export interface Admin {
     id: number;
     email: string;
     username: string;
+    password: string;
 }
 export async function adminLogin(data:adminLoginProbs) {
     const response = await axios.post(`${URL}/auth/login`, data);
@@ -28,7 +29,7 @@ export async function adminRegister(data: adminRegisterProbs) {
     const token = localStorage.getItem('token');
     if (!token) {
         console.error('No token found in localStorage');
-        return;
+        throw new Error('Unauthorized: No token found');
     }
     const response = await axios.post(`${URL}/admins`, data, {
         headers: {
@@ -51,6 +52,20 @@ export async function fetchAdminData(params?: Partial<Admin>) {
         },
     });
     console.log(response.data)
+    return response.data
+}
+
+export async function deleteAdmin(id: number) {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        console.error('No token found in localStorage');
+        return;
+    }
+    const response = await axios.delete(`${URL}/admins/${id}`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
     return response.data
 }
 
