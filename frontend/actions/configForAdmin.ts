315d04c4a672aaaead438e979535dd123df71960
MFarisA <muhfarisassami@gmail.com>
2025-04-25 00:53:15 +0700

    refactor: remove fallback URL for API; use environment variable only

--- frontend/actions/configForAdmin.ts
+++ frontend/actions/configForAdmin.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"; // Fallback for development
+const URL = process.env.NEXT_PUBLIC_API_URL; 
 
 export interface ConfigForAdmin {
     id: number;
