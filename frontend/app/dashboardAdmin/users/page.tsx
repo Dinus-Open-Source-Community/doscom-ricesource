428ce9571c79f2b6f8c70751a91c431c90010330
MFarisA <muhfarisassami@gmail.com>
2025-04-27 14:47:50 +0700

    feat: implement user fetching in admin user table; update data table to use fetched users

--- frontend/app/dashboardAdmin/users/page.tsx
+++ frontend/app/dashboardAdmin/users/page.tsx
@@ -1,20 +1,32 @@
 'use client'
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react';
 import { DataTable } from '@/components/admin/user-table/data-table';
-import { columns } from '@/components/admin/user-table/columns';
-import { users } from "@/components/admin/data/users"
+import { columnsUsers } from '@/components/admin/user-table/columns';
+import { fetchUserByAdmin, UserbyAdmin } from '@/actions/userByAdmin';
 
 export default function userTable() {
+  const [users, setUsers] = useState<UserbyAdmin[]>([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        const data = await fetchUserByAdmin();
+        setUsers(data);
+      } catch (error) {
+        console.error('Failed to fetch users:', error);
+      }
+    }
+    getUsers();
+  }, []);
+
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-blue-100" />
-            <div className="aspect-video rounded-xl bg-blue-100" />
-            <div className="aspect-video rounded-xl bg-blue-100" />
-          </div>
-          {/* <ConfigDataTable /> */}
-          <DataTable columns={columns} data={users} />
-        </div>
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="aspect-video rounded-xl bg-blue-100" />
+        <div className="aspect-video rounded-xl bg-blue-100" />
+        <div className="aspect-video rounded-xl bg-blue-100" />
+      </div>
+      <DataTable columns={columnsUsers} data={users} />
+    </div>
   );
 };
