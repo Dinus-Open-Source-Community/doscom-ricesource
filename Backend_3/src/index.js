42056ec3fa1bd05adebf6aea1c59975232800487
Naufal Adrian <87624905+naufaladrian@users.noreply.github.com>
2025-06-13 20:44:01 +0700

    Update index.js

--- Backend_3/src/index.js
+++ Backend_3/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ["http://localhost:5173", " http://localhost:3000"],
+  origin: ["http://localhost:5173", " http://localhost:3000","https://ricesource.doscom.org"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
